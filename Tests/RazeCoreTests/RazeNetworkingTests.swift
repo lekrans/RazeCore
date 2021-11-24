102e9c8758b8bbfdc98a6fbc1ee9796b429a6a36
lekrans <mlekrans@hotmail.com>
2021-11-24 10:53:15 +0100

    Refactored first Neetworking API to include dependency injection

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -9,19 +9,31 @@ import XCTest
 @testable import RazeCore
 
 final class RazeNetworkingTests: XCTestCase {
+    
+    class NetworkSessionMock: NetworkSession {
+        func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+            completionHandler(data, error)
+        }
+        
+        var data: Data?
+        var error: Error?
+        
+        
+    }
 
     func testLoadDataCall() {
         let manager = RazeCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
         let expectation = XCTestExpectation(description: "Called for data")
-        guard let url = URL(string: "https://raywenderlich.com") else {
-            return XCTFail("Could not create URL property")
-        }
-        
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnedData):
-                XCTAssertNotNil(returnedData, "Response data is nil")
+                XCTAssertEqual(data, returnedData, "Manager returned unexpected data")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
